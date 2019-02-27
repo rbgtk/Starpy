c872091a4d68a93bef36af9a0ee07a90c19bf01a
Robby Goetinck <robby.goetinck@outlook.com>
2019-02-27 19:37:12 +0100

    Reorganizing

---
+++ app/js/app.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const AJAX = (function() {
+    function doGet(resource) {
+        return new Promise(function (resolve, reject) {
+            $.get(resource).done(resolve).fail(reject);
+        });
+    }
+
+    function doPost(target, data) {
+        return new Promise(function (resolve, reject) {
+            $.post(target, data).done(resolve).fail(reject);
+        });
+    }
+
+    return {
+        get:  doGet,
+        post: doPost
+    }
+})();
+
+(function main() {
+    Wallpaper
+	.init()
+	.then(Wallpaper.start);
+})();
